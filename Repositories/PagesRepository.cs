1b545976fff6b0eb0fd6d6155cfdeaaa7dbc5e1d
otegecmis <177798054+otegecmis@users.noreply.github.com>
2024-09-11 15:24:29 +0300

    refactor: update placeholder `content` data in `pages repository`

--- Repositories/PagesRepository.cs
+++ Repositories/PagesRepository.cs
@@ -10,9 +10,21 @@ public class PagesRepository
     {
         _pages = new List<Page>()
         {
-            new Page() { Title = "Home", Content = "This is my home page." },
-            new Page() { Title = "Resume", Content = "This is my resume page." },
-            new Page() { Title = "Contact", Content = "This is my contact page." }
+            new Page()
+            {
+                Title = "Home",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse blandit tellus quis odio ullamcorper pulvinar. Maecenas ac magna velit. Sed mi est, cursus id mauris eu, mollis tincidunt nibh. Sed nisi dolor, ornare ac elementum non, laoreet vitae ex. Nulla dictum fringilla dapibus. Suspendisse lacus leo, fringilla sit amet augue id, maximus suscipit nunc. Morbi ullamcorper tortor leo, sed ultrices diam dictum id."
+            },
+            new Page()
+            {
+                Title = "Resume",
+                Content = "Vivamus ipsum urna, convallis viverra elementum sit amet, tempus id purus. Nunc sed semper mauris. Praesent lobortis leo nisl, a imperdiet ligula convallis ut. Etiam justo enim, scelerisque a volutpat a, tristique ac ligula."
+            },
+            new Page()
+            {
+                Title = "Contact",
+                Content = "Aenean dolor erat, venenatis a fringilla nec, aliquam non ex."
+            }
         };
     }
 
